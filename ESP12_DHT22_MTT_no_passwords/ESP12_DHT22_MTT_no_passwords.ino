3e3f9483085f2164bd797e6ed59ca85d5915a00f
jimbob4334 <j_ferree@yahoo.com>
2019-03-31 13:41:48 -0500

    Add files via upload

---
+++ ESP12_DHT22_MTT_no_passwords/ESP12_DHT22_MTT_no_passwords.ino
@@ -0,0 +1,117 @@
+// Get ESP8266 going with Arduino IDE
+// - https://github.com/esp8266/Arduino#installing-with-boards-manager
+// Required libraries (sketch -> include library -> manage libraries)
+// - PubSubClient by Nick ‘O Leary
+// - DHT sensor library by Adafruit
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+//#include "DHT.h"_
+#include <DHT.h>
+
+#define wifi_ssid "PerplexedMoustache24"
+#define wifi_password "wifi_password"
+
+#define mqtt_server "192.168.1.231"
+#define mqtt_user "JimBob_MQTT"
+#define mqtt_password "mqtt_password usual"
+
+#define humidity_topic "sensor/humidity01"
+#define temperature_topic "sensor/temperature01"
+
+#define DHTTYPE DHT22
+#define DHTPIN  2
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  ESP.wdtDisable();
+  ESP.wdtEnable(WDTO_8S);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+  return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+long lastMsg = 0;
+float temp = 0.0;
+float hum = 0.0;
+float diff = 1.0;
+
+void loop() {
+// Connect mqtt
+  if (!client.connected()) {
+    reconnect();
+  }
+  ESP.wdtFeed();
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+
+    float newTemp = dht.readTemperature();
+    float newHum = dht.readHumidity();
+
+    if (checkBound(newTemp, temp, diff)) {
+      temp = newTemp*9/5+32;
+      Serial.print("New temperature:");
+      Serial.println(String(temp).c_str());
+      client.publish(temperature_topic, String(temp).c_str(), true);
+    }
+
+    if (checkBound(newHum, hum, diff)) {
+      hum = newHum;
+      Serial.print("New humidity:");
+      Serial.println(String(hum).c_str());
+      client.publish(humidity_topic, String(hum).c_str(), true);
+    }
+  }
+}
